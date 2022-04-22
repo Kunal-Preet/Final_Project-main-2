c511eff84ffffcd440f6480b0402929bbc9b75c4
Kunal Preet <kunalpreet@Kunals-MacBook-Air.local>
2022-04-22 22:23:03 +0530

    commit1

--- script2.js
+++ script2.js
@@ -173,4 +173,61 @@ const queryHandler = function() {
         }
     });
 };
+
+const serviceWorkerNotify = async (title, msg) => {
+    const registration = await navigator.serviceWorker.ready;
+    if (registration) return registration.showNotification(title, msg);
+  }
+
+  const notify = (title, msg) => !msg?.actions ? new Notification(title, msg) : serviceWorkerNotify(title, msg);
+
+	const askPermission = async () => {
+	  // Is Web Notifications available on the browser
+	  if(!("Notification" in window)) {
+		console.error("Notification API is not available on this device!");
+		return false;
+	  }
+	
+	  // Did the user previously allow notifications
+	  if (Notification.permission === 'granted') {
+		return true;
+	  }
+	
+	  // If the user denied or hasn't been asked yet
+	  if (Notification.permission === 'denied' || Notification.permission === 'default') {
+		try {
+		  // Ask for permission
+		  const permission = await Notification.requestPermission();
+		  if (permission === 'granted') {
+			return true;
+		  }
+		  return false;
+		} catch (e) {
+		  console.error("There was an issue acquiring Notification permissions", e);
+		  return false;
+		}
+	  }
+	  return false;
+	}
+
+	const notifyAdvanced = async () => {
+		const permission = await askPermission();
+		if (permission) {
+		  const title = "Showing weather of the following location"
+		  const msg = {
+			badge: "assets/images/icon.png",
+			tag: 'location-request',
+			icon: 'assets/images/icon.png',
+			//image: 'transfer.png',
+			body: searchLine.value
+      
+		  }
+		  const rslt = notify(title, msg);
+		  console.log('Success!', rslt);
+      console.log(searchLine.value)
+		}
+	  }
+	
+
 searchBtn.addEventListener("click", queryHandler);
+
